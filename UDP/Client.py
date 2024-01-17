452a595c5e09417f044f58856ffc06d2d7618ffb
mariabdma <mariabma76@gmail.com>
2024-01-17 15:03:55 -0300

    comments

--- UDP/Client.py
+++ UDP/Client.py
@@ -2,35 +2,47 @@ from socket import *
 from threading import *
 import random
 
+# obtem o nome do host local e seu endereço IP e define a porta do servidor 
 host_name = gethostname()
 server_name = gethostbyname(host_name)
 server_port = 9999
 
+# cria um socket UDP
 client_socket = socket(AF_INET, SOCK_DGRAM)
+# atribui um endereço IP e porta local aleatória para o socket do cliente
 client_socket.bind((server_name, random.randint(8000, 9000)))
 
+# solicita ao usuário um apelido (nickname)
 name = input("NICKNAME: ")
 
+# função que recebe as mensagens do servidor 
 def receive():
     while True:
         try:
+            # recebe uma mensagem de no máximo 2048 bytes
             message, _ = client_socket.recvfrom(2048)
+            # exibe a mensagem decodificada
             print(message.decode())
         except:
             pass
 
-
+# cria uma thread para a função de receber e a inicia
 thread = Thread(target=receive)
 thread.start()
 
+# envia uma mensgame de boas-vindas ao servidor com o apelido fornecido pelo cliente
 client_socket.sendto(f"bem vindo: {name}".encode(), (server_name, server_port))
 
+# loop para enviar mensagens para o servidor
 while True:
+    # solicitação de mensagem ao cliente/usuário
     message = input("digite: ")
 
+    # verificação para, caso o usuário digitar !q, informar que ele saiu, fechar o socket e encerrar o programa
     if message == "!q":
         client_socket.sendto(f"{name}: saiu da sala".encode(), (server_name, server_port))
         client_socket.close()
         exit()
     else:
+        # envia a mensagem ao servidor no formato "apelido: mensagem"
         client_socket.sendto(f"{name}: {message}".encode(), (server_name, server_port))
--- UDP/Server.py
+++ UDP/Server.py
@@ -2,34 +2,45 @@ from socket import *
 from threading import *
 from queue import *
 
+# cria uma fila para armazenar as mensagens recebidas
 messages = Queue()
+# lista para armazenar os endereços dos clientes conectados
 clients = []
 
+# configurações do servidor 
 server_port = 9999
 host_name = gethostname()
 server_address = gethostbyname(host_name)
 
+# cria um socket udp para o servidor e atribui um endereço e uma porta
 server_socket = socket(AF_INET, SOCK_DGRAM)
 server_socket.bind((server_address, server_port))
+# imprime uma mensagem indicando que o servidor está pronto
 print("server is ready to receive")
 
+# função para receber mensagens dos clientes
 def receive():
     while True:
         try:
+            # recebe uma mensagem e o endereço do remetente e coloca a mensagem com endereço na fila
             message, addr = server_socket.recvfrom(2048)
             messages.put((message, addr))
         except:
             pass
 
-
+# função para transmitir mensagens para todos os clientes conectados
 def broadcast():
     while True:
+        # verifica se há mensagens na fila
         while not messages.empty():
+            # obtém a mensagem e o endereço do remetente, decodifica a mensagem e a exibe no servidor
             message, addr = messages.get()
             print(message.decode())
 
+           
             if addr not in clients:
                 clients.append(addr)
+            
             
             for client in clients:
                 try:
@@ -43,7 +54,8 @@ def broadcast():
                 except:
                     clients.remove(client)
 
-
+# cria duas threads para as funções receive e broadcast e as inicia
+first_thread = Thread(target=receive)
 first_thread = Thread(target=receive)
 second_thread = Thread(target=broadcast)
 
