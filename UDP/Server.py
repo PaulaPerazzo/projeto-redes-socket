942091d08ce98798f20458ad08c775a81f5a8f20
mbmartns <mbmartinspg@gmail.com>
2024-02-01 19:03:58 -0300

    corrigindo versão do servidor, garantindo que cliente não recebe a prórpia mensagem

--- UDP/Server.py
+++ UDP/Server.py
@@ -13,20 +13,18 @@ server_address = gethostbyname(host_name)
 
 server_socket = socket(AF_INET, SOCK_DGRAM)
 server_socket.bind((server_address, server_port))
-
-print("Servidor conectado!")
-
+print("Server is ready to receive")
 
 def receive():
     while True:
         try:
             message, addr = server_socket.recvfrom(1024)
             messages.put((message, addr))
-            # print(f"Mensagem recebida de {addr}")
+            #print(f"Mensagem recebida de {addr}")
         except Exception as e:
             print(f"Erro ao receber mensagem: {e}")
 
-
+        
 def process_message(decoded_message, addr):
     if addr not in [client[0] for client in clients]:
         if decoded_message.startswith("hi, meu nome eh "):
@@ -40,7 +38,7 @@ def handle_file(message, addr, name):
     formatted_message = f"{addr[0]}:{addr[1]}/~{name}: "
     lista_envios = []
     lista_envios.append(formatted_message)
-    while message != "\\x00":
+    while  message != "\\x00":
         lista_envios.append(message)
         message, _ = messages.get()
         message = message.decode("utf-8")
@@ -49,6 +47,7 @@ def handle_file(message, addr, name):
     lista_envios.append(current_time)
 
     return lista_envios
+
 
 
 def broadcast():
@@ -61,6 +60,9 @@ def broadcast():
 
         if new_client:
             envio.append(f"{new_client} entrou na sala")
+            server_socket.sendto("Você entrou da sala".encode(), addr)
+            server_socket.sendto("\\x00".encode(), addr)
+
         else:
             dicionario_clientes = dict(clients)
             nome = dicionario_clientes.get(addr)
@@ -74,13 +76,16 @@ def broadcast():
                 server_socket.sendto("\\x00".encode(), addr)
                 print((addr, nome))
                 clients.remove((addr, nome))
-
+                
         print(f'Esses são os clientes: {clients}')
         for client in clients:
             client_addr, _ = client
-            for pacote in envio:
-                server_socket.sendto(pacote.encode(), client_addr)
-            server_socket.sendto("\\x00".encode(), client_addr)
+            if client_addr == addr:
+                pass
+            else:
+                for pacote in envio:
+                    server_socket.sendto(pacote.encode(), client_addr)
+                server_socket.sendto("\\x00".encode(), client_addr)
 
 
 first_thread = Thread(target=receive)
